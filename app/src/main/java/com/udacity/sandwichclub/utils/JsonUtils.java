ebc3fe86a090a93c5c6d01a9ac61eca4463d1f41
U-DESKTOP-3S1A2JN\Johann <11635589+j-l5662@users.noreply.github.com>
2018-04-23 21:26:05 -0400

    test

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,18 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
+    public static Sandwich parseSandwichJson(String json) throws Exception {
+//        Sandwich Constructor
+//        (String mainName, List<String> alsoKnownAs, String placeOfOrigin,
+//                String description, String image, List<String> ingredients)
+        JSONObject obj = new JSONObject(json);
+
+
         return null;
     }
 }
